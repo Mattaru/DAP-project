ee8c691216a41ba4141ce4e267d1bd1e3ee91060
Mattaru <gctdljgkjnm1@gmail.com>
2024-11-20 18:24:38 +0200

    playwright.yml

--- e2e-playwright/playwright.config.js
+++ e2e-playwright/playwright.config.js
@@ -4,7 +4,7 @@ module.exports = {
   reporter: "list",
   workers: 5,
   use: {
-    baseURL: "http://drill-and-practice:7777",
+    baseURL: process.env.E2E_TESTS_LOCAL ? "http://host.docker.internal:7777" : "http://drill-and-practice:7777",
     headless: true,
     ignoreHTTPSErrors: true,
   },
