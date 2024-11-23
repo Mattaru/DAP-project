752d05bf061fc4a180deea6dbbd967d18b480a6f
Mattaru <gctdljgkjnm1@gmail.com>
2024-11-23 15:01:09 +0200

    Added g.evst to the db.js and added render.workflow

--- drill-and-practice/database/database.js
+++ drill-and-practice/database/database.js
@@ -1,13 +1,18 @@
 import { Pool } from "../deps.js";
 
 
+const DAP_DB_URL = Deno.env.get("DAP_DB_URL");
 const DATABASE_URL = Deno.env.get("DATABASE_URL");
-const FLYWAY_URL = Deno.env.get("FLYWAY_URL");
 const DENO_TEST = Deno.env.get("DENO_TEST");
+const FLYWAY_URL = Deno.env.get("FLYWAY_URL");
+const PRODUCTION = Deno.env.get("PRODUCTION");
+
 const CONCURRENT_CONNECTIONS = 10;
 let connectionPool;
 
-if (!DENO_TEST) {
+if (PRODUCTION) {
+  connectionPool = new Pool(DAP_DB_URL, CONCURRENT_CONNECTIONS, true);
+} else if (!DENO_TEST && !PRODUCTION) {
   if (DATABASE_URL) 
     connectionPool = new Pool(DATABASE_URL,CONCURRENT_CONNECTIONS, true);
   else if (FLYWAY_URL) 
